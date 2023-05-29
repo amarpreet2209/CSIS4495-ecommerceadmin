5fb396416d4b808d757d66634fe037262ab6d6d8
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-05-28 20:42:38 -0700

    image files are getting received at backend. Upload left

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -6,7 +6,9 @@ export default function ProductForm({
     _id,
     title: existingTitle,
     description: existingDescription,
-    price: existingPrice}) {
+    price: existingPrice,
+    images
+    }) {
     console.log("checking " +  existingTitle + ", " + existingDescription + ", " + existingPrice);
     const [title, setTitle] = useState(existingTitle || '');
     const [description, setDescription] = useState(existingDescription || '');
@@ -33,11 +35,49 @@ export default function ProductForm({
     if(goToProducts) {
         router.push('/products');
     }
+
+    async function uploadImages(ev) {
+        const files = ev.target?.files;
+
+        if (files?.length > 0) {
+            const data = new FormData();
+            for (const file of files) {
+                data.append('file', file);
+            }
+            // const res = await axios.post('/api/upload', data,{
+            //     headers: {'Content-Type': 'multipart/form-data'}
+            // });
+            const res = await fetch('/api/upload', {
+                method: 'POST',
+                body: data
+            })
+            console.log(res);
+        }
+    }
+
     return (
             <form onSubmit={saveProduct}>
 
                 <label>Product name</label>
                 <input type="text" placeholder="product name" value={title} onChange={ev => setTitle(ev.target.value)}/>
+                <label>Photos</label>
+                <div className="mb-2">
+                    <label className="w-24 h-24 text-center flex items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200 cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15" />
+                        </svg>
+                        <div>
+                            Upload
+                        </div>
+                        <input type="file" className="hidden" onChange={uploadImages}/>
+                    </label>
+
+                    {!images?.length && (
+                        <div>
+                            No photos in this product
+                        </div>
+                    )}
+                </div>
                 <label>Description</label>
                 <textarea placeholder="description" value={description} onChange={ev => setDescription(ev.target.value)}></textarea>
                 <label>Price (in USD)</label>
