d29532526cf8ff9b36fde24eb2f35a0efde6576c
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-05-28 19:09:45 -0700

    Product info updating now

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -2,25 +2,39 @@ import {useState} from "react";
 import {useRouter} from "next/router";
 import axios from "axios";
 
-export default function ProductForm({title: existingTitle, description: existingDescription, price: existingPrice}) {
+export default function ProductForm({
+    _id,
+    title: existingTitle,
+    description: existingDescription,
+    price: existingPrice}) {
     console.log("checking " +  existingTitle + ", " + existingDescription + ", " + existingPrice);
     const [title, setTitle] = useState(existingTitle || '');
     const [description, setDescription] = useState(existingDescription || '');
     const [price, setPrice] = useState(existingPrice || '');
     const [goToProducts, setGoToProducts] = useState(false);
     const router = useRouter();
+    console.log(_id);
 
-    async function createProduct(ev) {
+    async function saveProduct(ev) {
         ev.preventDefault();
+
+
         const data = {title, description, price};
-        await axios.post('/api/products', data);
+        // updating instead of creating
+        if(_id) {
+             await axios.put('/api/products', {...data,_id});
+        } else {
+            await axios.post('/api/products', data);
+        }
         setGoToProducts(true);
+
+
     }
     if(goToProducts) {
         router.push('/products');
     }
     return (
-            <form onSubmit={createProduct}>
+            <form onSubmit={saveProduct}>
 
                 <label>Product name</label>
                 <input type="text" placeholder="product name" value={title} onChange={ev => setTitle(ev.target.value)}/>
