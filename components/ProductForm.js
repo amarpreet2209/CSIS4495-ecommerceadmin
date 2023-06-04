a935c4d0815fad038fef9ed903c5c1ff35d38018
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-06-03 17:22:03 -0700

    Spinner added when uploading images

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -1,6 +1,7 @@
 import {useState} from "react";
 import {useRouter} from "next/router";
 import axios from "axios";
+import Spinner from "@/components/Spinner";
 
 export default function ProductForm({
     _id,
@@ -18,13 +19,15 @@ export default function ProductForm({
 
     const [goToProducts, setGoToProducts] = useState(false);
     const router = useRouter();
+
+    const [isUploading, setIsUploading] = useState(false);
     console.log(_id);
 
     async function saveProduct(ev) {
         ev.preventDefault();
 
 
-        const data = {title, description, price, images};
+        const data = {title, description, price, images  };
         // updating instead of creating
         if(_id) {
              await axios.put('/api/products', {...data,_id});
@@ -43,6 +46,7 @@ export default function ProductForm({
         const files = ev.target?.files;
 
         if (files?.length > 0) {
+            setIsUploading(true);
             const data = new FormData();
             for (const file of files) {
                 data.append('file', file);
@@ -55,6 +59,7 @@ export default function ProductForm({
             setImages(oldImages => {
                 return [...oldImages, ...res.data.links];
             })
+            setIsUploading(false);
         }
     }
 
@@ -64,12 +69,17 @@ export default function ProductForm({
                 <label>Product name</label>
                 <input type="text" placeholder="product name" value={title} onChange={ev => setTitle(ev.target.value)}/>
                 <label>Photos</label>
-                <div className="mb-2 flex flex-wrap gap-2">
+                <div className="mb-2 flex flex-wrap gap-1">
                     {!!images?.length && images.map(link => (
                         <div key={link} className={"h-24"}>
                             <img src={link} alt=""  className={"rounded-lg"}/>
                         </div>
                     ))}
+                    {isUploading && (
+                        <div className={"h-24 flex items-center"}>
+                            <Spinner/>
+                        </div>
+                    )}
                     <label className="w-24 h-24 text-center flex items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200 cursor-pointer">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15" />
@@ -79,12 +89,6 @@ export default function ProductForm({
                         </div>
                         <input type="file" className="hidden" onChange={uploadImages}/>
                     </label>
-
-                    {!images?.length && (
-                        <div>
-                            No photos in this product
-                        </div>
-                    )}
                 </div>
                 <label>Description</label>
                 <textarea placeholder="description" value={description} onChange={ev => setDescription(ev.target.value)}></textarea>
