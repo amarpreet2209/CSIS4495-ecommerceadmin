b9303595a531b0c391189657bf095e3158953be2
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-06-16 18:23:42 -0700

    Properties associated w/ Products

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -11,14 +11,16 @@ export default function ProductForm({
     price: existingPrice,
     images: existingImages,
     category: assignedCategory,
+    properties: assignedProperties,
     }) {
-    console.log("checking " +  existingTitle + ", " + existingDescription + ", " + existingPrice);
+    // console.log("checking " +  existingTitle + ", " + existingDescription + ", " + existingPrice);
     const [title, setTitle] = useState(existingTitle || '');
     const [description, setDescription] = useState(existingDescription || '');
 
     const [category, setCategory] = useState(assignedCategory || '');
 
     const [price, setPrice] = useState(existingPrice || '');
+    const [productProperties, setProductProperties] = useState(assignedProperties || {});
 
     const [images, setImages] = useState(existingImages || []);
 
@@ -34,12 +36,12 @@ export default function ProductForm({
         axios.get('/api/categories').then((result) => {
             setCategories(result.data);
         })
-    })
+    },[]);
     async function saveProduct(ev) {
         ev.preventDefault();
 
 
-        const data = {title, description, price, images, category };
+        const data = {title, description, price, images, category, properties:productProperties };
         // updating instead of creating
         if(_id) {
              await axios.put('/api/products', {...data,_id});
@@ -79,6 +81,26 @@ export default function ProductForm({
         setImages(images);
     }
 
+    const propertiesToFill = [];
+    if (categories.length > 0 && category) {
+        let catInfo = categories.find(({_id}) => _id===category);
+        propertiesToFill.push(...catInfo.properties,);
+
+        while(catInfo?.parent?._id) {
+            const parentCat = categories.find(({_id}) => _id===catInfo?.parent?._id)
+            propertiesToFill.push(...parentCat.properties)
+            catInfo = parentCat;
+        }
+    }
+
+    function setProductProp(propName, value) {
+        setProductProperties(prev => {
+            const newProdProps = {...prev};
+            newProdProps[propName] = value;
+            return newProdProps;
+        })
+    }
+
     return (
             <form onSubmit={saveProduct}>
 
@@ -95,6 +117,21 @@ export default function ProductForm({
                         <option value={c._id}>{c.name}</option>
                     ))}
                 </select>
+
+                {propertiesToFill.length > 0 && propertiesToFill.map(p => (
+                        <div className={"flex gap-1"}>
+                            <div>{p.name}</div>
+                            <select
+                                value={productProperties[p.name]}
+                                onChange={(ev) => setProductProp(p.name, ev.target.value)}>
+                                {p.values.map(v => (
+                                    <option value={v}>{v}</option>
+                                ))}
+                            </select>
+
+                        </div>
+                    )
+                )}
 
                 <label>Photos</label>
                 <div className="mb-2 flex flex-wrap gap-1">
