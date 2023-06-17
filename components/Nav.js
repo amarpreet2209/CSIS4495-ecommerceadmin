80ad197a6adddddc4b919fa768764ea03a904bd2
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-06-17 00:41:16 -0700

    Logout error resolved

--- components/Nav.js
+++ components/Nav.js
@@ -8,6 +8,12 @@ export default function Nav() {
     const router = useRouter();
     // console.log(router);
     const {pathname} = router;
+
+    async function logout() {
+        await router.push('/');
+        await signOut();
+    }
+
     return (
         <aside className={"text-white p-4 pr-0"}>
             <Link href={'/'} className={"flex gap-1 mb-4 mr-4"}>
@@ -50,7 +56,7 @@ export default function Nav() {
                     Settings
                 </Link>
                 <button
-                    onClick={() => signOut()}
+                    onClick={logout}
                     className={inactiveLink}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
