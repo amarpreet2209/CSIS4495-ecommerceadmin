ae0c3e951a22fc6fcee7cee2d8f92817d8747161
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-06-17 21:33:12 -0700

    Responsive Navbar and UI improved

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,10 @@
 import { useSession, signIn, signOut } from "next-auth/react"
 import Nav from "@/components/Nav";
+import {useState} from "react";
+import Logo from "@/components/Logo";
 
 export default function Layout({children}) {
+    const [showNav, setShowNav] = useState(false);
     const { data: session } = useSession();
 
     if (!session) {
@@ -14,12 +17,26 @@ export default function Layout({children}) {
         );
     }
     return (
-        <div className={"bg-bgGray min-h-screen flex"}>
-            <Nav/>
-            <div className={"bg-white flex-grow mt-1 mr-2 mb-2  rounded-lg p-4"}>
-                {children}
-            </div>
 
+        <div className={"bg-bgGray min-h-screen"}>
+            <div className={"block md:hidden flex items-center p-4"}>
+                <button onClick={() => setShowNav(true)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>
+                </button>
+                <div className={"flex grow justify-center mr-6"}>
+                    <Logo/>
+                </div>
+            </div>
+            <div className={"flex"}>
+                <Nav show={showNav}/>
+                <div className={"flex-grow p-4"}>
+                    {children}
+                </div>
+
+            </div>
         </div>
+
     );
 }
