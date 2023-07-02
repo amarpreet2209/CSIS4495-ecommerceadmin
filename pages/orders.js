485445fb75a59c1d8b116c6ebbcebfddc5df1d7e
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-07-02 15:11:24 -0700

    Table styling fixed, Orders API added

--- pages/orders.js
+++ pages/orders.js
@@ -1,9 +1,26 @@
 import Layout from "@/components/Layout";
+import {useEffect} from "react";
 
 export default function OrdersPage() {
+    
+    useEffect(() => {
+    
+    },[]);
+    
     return (
         <Layout>
-            <h1>Orders</h1>
+            <table className={"basic"}>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Recipient</th>
+                        <th>Products</th>
+                    </tr>
+                </thead>
+                <tbody>
+                
+                </tbody>
+            </table>
         </Layout>
     )
 }
