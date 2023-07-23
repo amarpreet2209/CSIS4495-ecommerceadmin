5cfffe0fdb07d55eda9cd6c73128ab436f56f158
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-07-23 12:38:27 -0700

    Spinner added to Edit Product Page

--- pages/products/edit/[...id].js
+++ pages/products/edit/[...id].js
@@ -3,24 +3,32 @@ import {useRouter} from "next/router";
 import {useEffect, useState} from "react";
 import axios from "axios";
 import ProductForm from "@/components/ProductForm";
+import Spinner from "@/components/Spinner";
 
 export default function EditProductPage() {
     const [productInfo, setProductInfo] = useState(null);
+    const [isLoading,setIsLoading] = useState(false);
+    
     const router = useRouter();
     const {id} = router.query;
     if(!id) {
         return;
     }
     useEffect(() => {
+        setIsLoading(true);
         axios.get('/api/products?id=' + id).then((response) => {
             setProductInfo(response.data);
-            console.log('setProductInfo +' + JSON.stringify(response.data));
+            // console.log('setProductInfo +' + JSON.stringify(response.data));
+            setIsLoading(false);
             }
         )
     },[id]);
     return (
         <Layout>
             <h1>Edit Product</h1>
+            {isLoading && (
+                <Spinner/>
+            )}
             {productInfo &&  <ProductForm {...productInfo}/>}
         </Layout>
     )
