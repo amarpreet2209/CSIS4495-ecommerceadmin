c48df3aaadcc2df1eeefa9db7b982b4fe0aa7bea
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-07-25 20:50:56 -0700

    Shipping fee added to settings

--- pages/settings.js
+++ pages/settings.js
@@ -7,69 +7,73 @@ import {withSwal} from "react-sweetalert2";
 function SettingsPage({swal}) {
     const [products, setProducts] = useState([]);
     const [featuredProductId, setFeaturedProductId] = useState('');
-    const [productsLoading, setProductsLoading] = useState(false);
-    const [featuredLoading, setFeaturedLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [shippingFee, setShippingFee] = useState('');
     
     useEffect(() => {
-        setProductsLoading(true);
-        
-        axios.get('/api/products').then((response) => {
-            setProducts(response.data);
-            setProductsLoading(false);
-        })
-        
-        setFeaturedLoading(true);
-        axios.get('/api/settings?name=featuredProductId').then((response) => {
-            setFeaturedProductId(response.data.value);
-            setFeaturedLoading(false);
-        })
-        
-    },[])
+        setIsLoading(true);
+        fetchAll().then(() => {
+            setIsLoading(false);
+        });
+    }, []);
     
+    async function fetchAll(){
+        await axios.get('/api/products').then(res => {
+            setProducts(res?.data);
+        });
+        await axios.get('/api/settings?name=featuredProductId').then(res => {
+            setFeaturedProductId(res?.data?.value);
+        });
+        await axios.get('/api/settings?name=shippingFee').then(res => {
+            setShippingFee(res?.data?.value);
+        });
+    }
     
     async function saveSettings() {
+        setIsLoading(true);
         await axios.put('/api/settings', {
             name: 'featuredProductId',
-            value: featuredProductId
-        }).then(() => {
-            swal.fire({
-                title: 'Settings saved!',
-                icon: 'success'
-            })
-        })
+            value: featuredProductId,
+        });
+        await axios.put('/api/settings', {
+            name: 'shippingFee',
+            value: shippingFee,
+        });
+        setIsLoading(false);
+        await swal.fire({
+            title: 'Settings saved!',
+            icon: 'success',
+        });
     }
     
     return (
         <Layout>
             <h1>Settings</h1>
-            {(productsLoading || featuredLoading) && (
-                <Spinner/>
+            {isLoading && (
+                <Spinner />
             )}
-            {(!productsLoading && !featuredLoading) && (
+            {!isLoading && (
                 <>
-                    <label>Featured Products</label>
-                    <select
-                        value={featuredProductId}
-                        onChange={ev => setFeaturedProductId(ev.target.value)}>
+                    <label>Featured product</label>
+                    <select value={featuredProductId} onChange={ev => setFeaturedProductId(ev.target.value)}>
                         {products.length > 0 && products.map(product => (
                             <option value={product._id}>{product.title}</option>
                         ))}
-                        
                     </select>
-                    
+                    <label>Shipping price (in usd)</label>
+                    <input type="number"
+                           value={shippingFee}
+                           onChange={ev => setShippingFee(ev.target.value)}
+                    />
                     <div>
-                        <button
-                            onClick={saveSettings}
-                            className={"btn-primary"}>Save Settings</button>
+                        <button onClick={saveSettings} className="btn-primary">Save settings</button>
                     </div>
                 </>
             )}
-            
-           
         </Layout>
-    )
+    );
 }
 
 export default withSwal(({swal}) => (
-    <SettingsPage swal={swal}/>
-))
+    <SettingsPage swal={swal} />
+));
