eb3203e3615693ccb6be37d5300d908d4d9de45a
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-07-26 23:49:48 -0700

    Shipping price (in CAD)

--- pages/settings.js
+++ pages/settings.js
@@ -60,7 +60,7 @@ function SettingsPage({swal}) {
                             <option key={product._id} value={product._id}>{product.title}</option>
                         ))}
                     </select>
-                    <label>Shipping price (in usd)</label>
+                    <label>Shipping price (in CAD)</label>
                     <input type="number"
                            value={shippingFee}
                            onChange={ev => setShippingFee(ev.target.value)}
