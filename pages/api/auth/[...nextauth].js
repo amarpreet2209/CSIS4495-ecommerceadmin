6c941d1497a0e7deefb9ef58e97a00e313a91963
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-07-26 22:51:36 -0700

    auth api updated

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,41 +3,36 @@ import GoogleProvider from 'next-auth/providers/google'
 import {MongoDBAdapter} from "@next-auth/mongodb-adapter";
 import clientPromise from "@/lib/mongodb";
 import {Admin} from "@/models/Admin";
+import {mongooseConnect} from '@/lib/mongoose';
 
 async function isAdminEmail(email) {
-    try {
-        const admin = await Admin.findOne({ email });
-        return !!admin; // Return true if admin is found, false otherwise
-    } catch (error) {
-        console.error('Error checking if email is admin:', error);
-        throw new Error('Server Error'); // Throw an error to be handled by the calling function
-    }
+    mongooseConnect();
+    return !! (await Admin.findOne({email}));
 }
 
 export const authOptions = {
-    secret: process.env.SECRET,
     providers: [
-
         GoogleProvider({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_SECRET
-        })
+        }),
     ],
     adapter: MongoDBAdapter(clientPromise),
     callbacks: {
-        session : async ({session, account, user}) => {
+        session: async ({session,token,user}) => {
             if (await isAdminEmail(session?.user?.email)) {
                 return session;
             } else {
                 return false;
             }
-        }
-    }
+        },
+    },
 };
+
 export default NextAuth(authOptions);
 
-export async function isAdminRequest(req, res) {
-    const session = await getServerSession(req, res, authOptions);
+export async function isAdminRequest(req,res) {
+    const session = await getServerSession(req,res,authOptions);
     if (!(await isAdminEmail(session?.user?.email))) {
         res.status(401);
         res.end();
